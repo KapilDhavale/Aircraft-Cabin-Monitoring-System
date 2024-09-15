9d439979c0fd603a16c5e00f801268e8b6b42046
Kapil Dhavale <kapildhavale602@gmail.com>
2024-09-16 02:16:15 +0530

    New Updates

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database'; // Import getDatabase
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBK2pCmtWpVd0L4Y4DWuEvJ1dGi4ByIz4s',
-  authDomain: 'aircraft-monitoring-syst-dc5dc.firebaseapp.com',
-  databaseURL: 'https://aircraft-monitoring-syst-dc5dc-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'aircraft-monitoring-syst-dc5dc',
-  storageBucket: 'aircraft-monitoring-syst-dc5dc.appspot.com',
-  messagingSenderId: '320070089409',
-  appId: '1:320070089409:web:662f5a82fcee4d9e00b240',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
